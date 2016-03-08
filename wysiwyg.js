f6aa706972ca2557a62f6b462e60a5bf76b89c30
Abbie Holder <abigail.c.holder@gmail.com>
2016-03-07 20:06:30 -0600

    final working code

--- wysiwyg.js
+++ wysiwyg.js
@@ -2,7 +2,7 @@ var people = [
   one = {
     title: "Samurai",
     name: "Tomoe Gozen",
-    bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
+    bio: "Tomoe was one of Minamoto Yoshinaka's finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
     image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
     lifespan: {
       birth: 1747,
@@ -10,9 +10,9 @@ var people = [
     }
   },
   two = {
-    title: "Princess",
+    title: "Princess of Wales",
     name: "Diana Frances",
-    bio: "......",
+    bio: "Diana, Princess of Wales, was the first wife of Charles, Prince of Wales, who is the eldest child and heir apparent of Queen Elizabeth II.",
     image: "http://images.closerweekly.com/uploads/posts/image/61392/princess-diana.jpg",
     lifespan: {
       birth: 1961,
@@ -20,23 +20,23 @@ var people = [
     }
   },  
   three = {
-    title: "Samurai",
-    name: "Tomoe Gozen",
-    bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-    image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+    title: "CIA Agent",
+    name: "Sydney Bristow",
+    bio: "Sydney is a highly skilled and well-established agent for the United States government.",
+    image: "http://ia.media-imdb.com/images/M/MV5BMTk5NTk5NDA5OV5BMl5BanBnXkFtZTYwMzA2ODEz._V1_SX640_SY720_.jpg",
     lifespan: {
-      birth: 1747,
-      death: 1797
+      birth: 1975,
+      death: "Present"
     }
   },  
   four = {
-    title: "Samurai",
-    name: "Tomoe Gozen",
-    bio: "Serving under Minamoto Yoshinaka, Tomoe was one of his finest soldiers, and her skills in battle dwarfed many of those held by even the strongest men in her unit.",
-    image: "https://upload.wikimedia.org/wikipedia/commons/4/48/Tomoe-Gozen.jpg",
+    title: "Great Pyrenees",
+    name: "Sansa",
+    bio: "The Pyrenean Mountain Dog, known as the Great Pyrenees in North America, is a large breed of dog used as a livestock guardian dog.",
+    image: "http://sitmeanssit.com/dog-training-mu/austin-dog-training/files/2011/07/photo.jpg",
     lifespan: {
-      birth: 1747,
-      death: 1797
+      birth: 2015,
+      death: "Present"
     }
   }
 ];
@@ -44,35 +44,31 @@ var people = [
 var personEl = document.getElementById("personContainer");
 
 for (var i = 0; i < people.length; i++) {
-  personEl.innerHTML += '<article class="people"><header>' + people[i].name + ', ' + people[i].title + '</header><section><p class="bioSection">' + people[i].bio + '</p><p><img src=' + people[i].image + '></section><footer>' + people[i].lifespan.birth + ' - '  + people[i].lifespan.death + '</footer></article>';
+  personEl.innerHTML += '<article class="people"><header>' + people[i].name + ', ' + people[i].title + '</header><section><p>' + people[i].bio + '</p><p><img src=' + people[i].image + '></section><footer>' + people[i].lifespan.birth + ' - '  + people[i].lifespan.death + '</footer></article>';
 };
 
 var inputEl = document.getElementById("bioEdit");
-// var specBio = document.getElementByClassName("bioSection").firstChild;
 var pplContainer = document.getElementsByTagName("article");
 var clickedEl = document.getElementsByClassName("clicked");
-var bio = document.getElementsByClassName("bioSection");
-
 
 // adds dotted border to container regardless of where you click inside the container -- adds dotted border
 personEl.addEventListener("click", function(e) {
   for (var i = 0; i < pplContainer.length; i++) {
-      pplContainer[i].classList.remove("clicked");
+    pplContainer[i].classList.remove("clicked");
   }
   e.target.closest("article").classList.add("clicked");
 });
 
-// focus on input whenever a person element is clicked
+// focus on input whenever a person element is clicked, also clears previous input value
 personEl.addEventListener("click", function(e) {
+  inputEl.value = "";
   inputEl.focus();
 });
 
-// when user begins typing in the input box, bio is mirrored to input (but only works first element so far, regardless of which is clicked)
+// when user begins typing in the input box, bio is mirrored to input
 inputEl.addEventListener("keyup", function(e) {
-  clickedEl.innerHTML = e.target.value;
-  console.log(clickedEl);
-  console.log(pplContainer);
-  console.log(bio);
+  var clicked = clickedEl[0].children[1].children[0];
+  clicked.innerHTML = e.target.value;
 });
 
 // clear input field on enter key press
